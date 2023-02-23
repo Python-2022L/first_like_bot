3dca10809fe2da1b6ef4c06700330885b5fe7251
JavohirJalilov <javohirjaliloff@gmail.com>
2023-02-23 18:43:52 +0500

    read and write

--- bot.py
+++ bot.py
@@ -1,20 +1,21 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 import os
-
-count_like = 0
-count_dislike = 0
+import json
 
 TOKEN=os.environ.get('TOKEN')
 
-
 def start(update: Update, context:CallbackContext):
     bot = context.bot
-
     chat_id = update.message.chat.id
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
 
-    btn1 = KeyboardButton(text=f"👍 {count_like}")
-    btn2 = KeyboardButton(text=f"👎 {count_dislike}")
+    like = data['like']
+    dislike = data['dislike']
+
+    btn1 = KeyboardButton(text=f"👍 {like}")
+    btn2 = KeyboardButton(text=f"👎 {dislike}")
 
     keyboard = ReplyKeyboardMarkup([[btn1, btn2]], resize_keyboard=True)
 
@@ -27,20 +28,27 @@ def like_and_dislike(update: Update, context: CallbackContext):
     chat_id = update.message.chat.id
     text = update.message.text[0]
 
-    global count_like
-    global count_dislike
+    with open('data.json', 'r') as f:
+        data = json.loads(f.read())
+
+    like = data['like']
+    dislike = data['dislike']
 
     if text == "👍":
-        count_like += 1
+        like += 1
 
     if text == "👎":
-        count_dislike += 1
+        dislike += 1
 
-    btn1 = KeyboardButton(text=f"👍 {count_like}")
-    btn2 = KeyboardButton(text=f"👎 {count_dislike}")
+    data['like'] = like
+    data['dislike'] = dislike
+
+    with open('data.json', 'w') as f:
+        json.dump(data,fp=f, indent=4)
+    btn1 = KeyboardButton(text=f"👍 {like}")
+    btn2 = KeyboardButton(text=f"👎 {dislike}")
 
     keyboard = ReplyKeyboardMarkup([[btn1, btn2]], resize_keyboard=True)
-
 
     bot.sendMessage(chat_id, "LIKE and DISLIKE", reply_markup=keyboard)
  
